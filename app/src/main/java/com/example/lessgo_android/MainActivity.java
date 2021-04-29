51bb570b8488be6fa1caaefb56e0cbb406615648
Patricia <test.conceptdevapp@gmail.com>
2021-04-29 10:08:41 +0200

    modif xlm

--- app/src/main/java/com/example/lessgo_android/MainActivity.java
+++ app/src/main/java/com/example/lessgo_android/MainActivity.java
@@ -8,16 +8,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.lessgo_android.model.UserBean;
-
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profil);
 
             // Test appel getPositions()
             /*Thread thread = new Thread() {
